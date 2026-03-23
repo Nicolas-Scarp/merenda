8aaa4553f45affac0de51f03415547af29589bc6
Nicolas Scarp <nickscarptravassos@gmail.com>
2026-03-23 16:43:06 -0300

    feature: ideia de telas

---
+++ merenda.py
@@ -0,0 +1,20 @@
+#TELA 1
+                #SEC. EDUCAÇÃO
+                #   MERENDA
+                #ESTOQUE ESCOLAR.
+
+        #QUANTIDADE ESTOQUE ATUAL. '''Clicla e visualiza tudo'''
+
+            #VIZUALIZAR PRODUTOS.     #CADASTRAR PRODUTOS.
+
+
+#TELA 2
+        #VISUALIZAR PRODUTOS. '''Clica, vai para tela de vizualização'''
+
+        #QUANTIDADE     #NOME   #INFORMAÇÕES
+
+#TELA 3
+        #EDITAR ESTOQUE. '''Clica, vai para tela de cadastro'''
+
+        #NOME #CADASTRO #EDITAR #EXCLUIR
+
